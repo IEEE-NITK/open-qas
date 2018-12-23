1544826e9d7c6147f4857bda28b12047d69b13c8
Gurupungav Narayanan <gurupungavn@gmail.com>
2018-12-23 23:12:52 +0400

    Squashed bugs

--- openqas/reader/reader.py
+++ openqas/reader/reader.py
@@ -8,14 +8,14 @@ from ..utils.data import WordEmbeddings
 
 def QuestionEncoder(lstm_nodes=128, seq_length=1000, embeddings_dim=100):
     inputs = Input((seq_length, embeddings_dim))
-    hidden_layer = Bidirectional(LSTM(lstm_nodes, return_state=True, return_sequences=False))(inputs)
+    hidden_layer = Bidirectional(LSTM(lstm_nodes, return_state=False, return_sequences=False))(inputs)
     model = Model(inputs=inputs, outputs=hidden_layer)
 
     return model
 
 def ParagraphEncoder(lstm_nodes=256, seq_length=1000, embeddings_dim=100):
     inputs = Input((seq_length, embeddings_dim))
-    hidden_layer = Bidirectional(LSTM(lstm_nodes, return_state=True, return_sequences=False))(inputs)
+    hidden_layer = Bidirectional(LSTM(lstm_nodes, return_state=False, return_sequences=False))(inputs)
     model = Model(inputs=inputs, outputs=hidden_layer)
 
     return model
@@ -30,14 +30,14 @@ class Reader:
 
         self.embedder = Embedder(self.glove_embeddings, seq_length=seq_length, stopwords=stopwords)
 
-        self.question_encoder = QuestionEncoder()
-        self.paragraph_encoder = ParagraphEncoder()
+        self.question_encoder = QuestionEncoder(seq_length=seq_length)
+        self.paragraph_encoder = ParagraphEncoder(seq_length=seq_length)
     
-    def predict(self, paragraphs, questions):
+    def encode(self, paragraphs, questions):
         q_input = self.embedder.get_embeddings(questions)
         p_input = self.embedder.get_embeddings(paragraphs)
 
         q_prediction = self.question_encoder.predict(q_input)
         p_prediction = self.paragraph_encoder.predict(p_input)
 
-        return p_prediction, q_prediction
+        return (p_prediction), (q_prediction)
