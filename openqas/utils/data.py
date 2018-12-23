ffdf093680b67bf996c393e099d805c50f0d345f
Gurupungav Narayanan <gurupungavn@gmail.com>
2018-12-23 21:03:06 +0400

    Changes

--- openqas/utils/data.py
+++ openqas/utils/data.py
@@ -6,7 +6,7 @@ from fuzzywuzzy import fuzz, StringMatcher
 
 class WordEmbeddings:
     """
-    Module sto load and handle the GloVe Word Embeddings
+    Module to load and handle the GloVe Word Embeddings
     """
 
     def __init__(self):
@@ -14,7 +14,7 @@ class WordEmbeddings:
         self.word_to_vec = {}
         self.word_to_index = {}
         self.index_to_word = {}
-
+    
     def load_glove(self, path):
         """
         Loads a pretrained GloVe model
@@ -28,9 +28,20 @@ class WordEmbeddings:
                 self.word_to_vec[line[0]] = np.array(line[1:], dtype='float64')
     
             for x,y in enumerate(sorted(self.vocabulary)):
-                self.word_to_index[y] = x
-                self.index_to_word[x] = y
+                self.word_to_index[y] = x+1
+                self.index_to_word[x+1] = y
         
+        self.EMBEDDING_DIM = len(self.word_to_vec['the'])
+        
+    def get_matrix(self):
+        embedding_matrix = np.zeros((len(self.word_to_index) + 1, self.EMBEDDING_DIM))
+        for word, i in self.word_to_index.items():
+            embedding_vector = self.word_to_vec[word]
+            if(embedding_vector is not None):
+                embedding_matrix[i] = embedding_vector
+
+        return embedding_matrix
+
     def in_vocab(self, word):
         """
         Checks if a word is present in the vocabulary
